0e729ffc9755b9c5e97323e62a315594f623436a
guim0 <guimodev@gmail.com>
2023-12-11 18:09:19 -0300

    Feat: Ajustando imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CharactersModule } from './characters/characters.module';
-import { CharactersService } from './characters/characters.service';
 
 @Module({
   imports: [CharactersModule],
   controllers: [],
-  providers: [CharactersService],
+  providers: [],
 })
 export class AppModule {}
